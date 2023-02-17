8616d9ee1d2a0dd3253a20fa225ac7fcf3b49e75
root <6281@holbertonstudents.com>
2023-02-17 09:46:21 -0800

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/* main: - afficher main */
+
+/**
+ * main - entry point
+ * Puts: - afficher texte
+ * Return: - fin script
+ */
 int main(void)
-/* Puts: - afficher texte */
-/* Return: - fin script */
 {puts("\"Programming is like building a multilingual puzzle");
 return (0); }
