9df617bc8109840b651a6dedc94041317cf7cd83
root <6281@holbertonstudents.com>
2023-02-20 09:32:54 -0800

    task 1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,13 +22,13 @@ int main(void)
 	}
 	else
 	{
-		if (n > 5)
+		if (n <= 5)
 		{
-			printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
+			printf("Last digit of %d is %d and is less than 6 and 0\n", n, lastdigit);
 		}
 		else
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+			printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 		}
 	}
 	return (0);
