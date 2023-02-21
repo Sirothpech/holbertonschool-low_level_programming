d849aec75bb24ca7949a530a98284d07f0335754
root <6281@holbertonstudents.com>
2023-02-21 07:20:06 -0800

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Putchar - description
+ * Return: fin du script
+ */
+
+int main(void)
+{
+	int print_alphabet;
+
+	for (print_alphabet = 'a'; print_alphabet <= 'z'; print_alphabet++)
+
+		_putchar (print_alphabet);
+
+	_putchar ('\n');
+
+	return (0);
+}
