ab6f3dcada038200b656ae4fd7fd7b203708b1ed
root <6281@holbertonstudents.com>
2023-02-21 14:08:31 -0800

    task 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - lower and upper case
+ * @c: char verif
+ * Return: fin du script
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+
+}
