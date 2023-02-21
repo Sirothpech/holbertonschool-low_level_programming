597569af09b88d503f41d0a5652df5f643f4ebda
root <6281@holbertonstudents.com>
2023-02-20 16:57:43 -0800

    task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,12 +13,12 @@ int main(void)
 
 	for (i = '0' ; i <= '9' ; i++)
 	{
-		putchar(i);
+	putchar(i);
 	if (i != '9')
-		     {
-			     putchar(',');
-			     putchar(' ');
-		     }
+	{
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
