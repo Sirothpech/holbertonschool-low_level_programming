ebf0f419cb0724deb7960a1442ba5964387a92b6
root <6281@holbertonstudents.com>
2023-02-20 16:16:48 -0800

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: fin script
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
