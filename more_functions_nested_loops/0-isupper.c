6e79ea56e5da0151b5477729279b716b8736d0ae
root <6281@holbertonstudents.com>
2023-02-24 02:59:42 -0800

    task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * isupper - check upppercase
+ * return: 1 if c upper 0 if false
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
