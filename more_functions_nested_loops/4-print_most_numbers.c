3b799be262f26528a019c52e1a5566d0a3d5d68b
root <6281@holbertonstudents.com>
2023-02-24 05:58:28 -0800

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - description
+ * Return: fin du script
+ */
+
+void print_most_numbers(void)
+{
+	char alphab;
+
+	for (alphab = '0'; alphab <= '9'; alphab++)
+	{
+		if (alphab != '2' && alphab != '4')
+
+		_putchar (alphab);
+	}
+
+	_putchar ('\n');
+
+}
