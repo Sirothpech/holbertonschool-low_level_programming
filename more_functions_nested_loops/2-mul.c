965ba3b90cff5ef112572fee8403eb28968cd423
root <6281@holbertonstudents.com>
2023-02-24 03:54:54 -0800

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiples two integers
+ * @a: char a
+ * @b: char b
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	if ((a <= 0 || a >= 0) && (b <= 0 || b >= 0))
+	{
+		return (a * b);
+	}
+}
