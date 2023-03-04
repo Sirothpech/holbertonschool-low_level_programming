72916fc670905dd0dad5bd226d8cc9d080e90561
root <6281@holbertonstudents.com>
2023-03-04 15:43:10 -0800

    task 6/7 p3

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_chessboard - affiche echec
+ * @a: array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar (a[x][y]);
+		_putchar ('\n');
+	}
+}
