f93bf239e30fe6c7b53b1b8bf69e12239f0b6965
root <6281@holbertonstudents.com>
2023-03-07 14:38:13 -0800

    task 0

--- static_libraries/6-abs.c
+++ static_libraries/6-abs.c
@@ -1,23 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-/**
- * _abs - valeur absolue
- * @n: char verif
- * Return: fin du script
- */
-
-int _abs(int n)
-{
-	if (n < 0)
-	{
-		int val_abs;
-
-		val_abs = n * -1;
-		return (val_abs);
-	}
-	return (n);
-}#include "main.h"
-#include <stdio.h>
  /**
   * _abs - valeur absolue
   * @n: char verif
