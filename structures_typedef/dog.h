addcf63e37f3b97a8e7bc90e786b400620a133a4
root <6281@holbertonstudents.com>
2023-03-20 07:56:00 -0700

    readme task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - id dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: create a id dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
