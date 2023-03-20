b1a44041625ede83b867bf46c29c8fd62b24a4d2
root <6281@holbertonstudents.com>
2023-03-20 12:00:49 -0700

    task 5

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - print new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog = 
