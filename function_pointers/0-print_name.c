10f45d97646adef08804b6ea6dd40ca62edb3510
root <6281@holbertonstudents.com>
2023-03-21 08:42:53 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - print name upper or not
+ * @name: name check
+ * @f: call funct
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
