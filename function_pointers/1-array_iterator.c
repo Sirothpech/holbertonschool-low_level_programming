46c7755b32bc0a8a80202073b27bee31783ca965
root <6281@holbertonstudents.com>
2023-03-22 02:28:48 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - print each element of an array
+ * @array: array
+ * @size: size array
+ * @action: call funct
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL)
+		for( i = 0; i < size; i++)
+			(*action)(array[i]);
+}
