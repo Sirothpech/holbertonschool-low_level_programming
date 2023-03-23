c1545f5ec9ef20b5c7e13b80c976a280f3eb4193
root <6281@holbertonstudents.com>
2023-03-23 05:42:43 -0700

    task 3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,25 +1,59 @@
 #include "3-calc.h"
 #include <stdlib.h>
+/**
+ * op_add - addition
+ * @a: variable
+ * @b: second variable
+ * Return: result;
+ */
 
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - subtraction
+ * @a: variable
+ * @b: second variable
+ * Return: result;
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - multiplication
+ * @a: variable
+ * @b: second variable
+ * Return: result
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - division
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - modulo
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
 
 int op_mod(int a, int b)
 {
