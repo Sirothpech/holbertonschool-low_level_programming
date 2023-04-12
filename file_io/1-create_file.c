26fc97fb19317f1cbdc962005e95f85dd187f419
root <6281@holbertonstudents.com>
2023-04-12 07:00:40 -0700

    Fix: the if condition about the length string

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -45,7 +45,6 @@ int create_file(const char *filename, char *text_content)
 		write(fd, text_content, len);
 		if (len == -1)
 		{
-			write(STDERR_FILENO, "fails\n", 6);
 			close(fd);
 			return (-1);
 		}
