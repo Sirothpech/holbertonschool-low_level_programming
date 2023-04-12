d05bde84f659d7326c281271b72d9e11b91d4733
root <6281@holbertonstudents.com>
2023-04-12 06:58:55 -0700

    Fix: the if condition about the length string

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int _strlen(char *s)
 
 /**
  * create_file - function that creates a file
- * @filemane: name of the file to create
+ * @filename: name of the file to create
  * @text_content: string to write into the file
  * Return: 1 or -1 if failed
  */
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
