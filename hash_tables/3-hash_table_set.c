a456572af2ca1172c96ad13c32d8b9b36715ca46
root <6281@holbertonstudents.com>
2023-04-14 08:31:17 -0700

    Add: function that adds an element to the hash table.

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -35,6 +35,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (!strcmp(ptr->key, key))
 		{
+			ptr->value = strdup(value);
 			free(new_node);
 			return (1);
 		}
