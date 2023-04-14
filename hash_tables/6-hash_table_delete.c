e063716b8e9eb336a3a2de9ca5e131a6f8060f66
root <6281@holbertonstudents.com>
2023-04-14 11:47:41 -0700

    task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -4,10 +4,12 @@
  * hash_table_delete - function that deletes a hash table
  * @ht: hash table to delete
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
 	hash_node_t *tmp2 = NULL, *tmp = NULL;
+
 	if (ht == NULL)
 		return;
 	if (ht->array != NULL)
