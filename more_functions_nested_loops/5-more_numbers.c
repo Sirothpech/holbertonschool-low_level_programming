d62102d1c8b9e4647810cc3b34cc0306954411b8
root <6281@holbertonstudents.com>
2023-05-03 05:02:09 -0700

    Fix: redoing this exercice to review and better understand it

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
  * more_numbers - description
- * Return: fin du script
+ * Return: void
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	i = j = 0;
+	i = 0;
+	j = 0;
 
 	while (i < 10)
 	{
@@ -17,7 +18,7 @@ void more_numbers(void)
 			if (j >= 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
-			++j;
+			j++;
 		}
 		_putchar('\n');
 		j = 0;
