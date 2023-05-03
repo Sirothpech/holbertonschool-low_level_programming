6d6c8ae3109c0727ff24b08b6e2f27714e0c3739
root <6281@holbertonstudents.com>
2023-05-03 07:08:38 -0700

    Fix: redoing the code to read more clearly

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,25 @@
 #include "main.h"
+
 /**
  * print_square - draws a square using multiple terminal lines and #'s.
  * @n: the number of # to print in each line.
  */
-void print_square(int n)
+
+void print_square(int size)
 {
 	int i, j;
 
-	if (n > 0)
+	if (size <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-				_putchar('#');
-			_putchar('\n');
-		}
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+
 }
